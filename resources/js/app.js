fc7599caeafbfa2fcef0f799e2cf29a1e3d4590b
buguenocesar92 <buguenocesar92@gmail.com>
2025-09-09 22:01:50 -0300

    fix galery

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,9 @@
 import './bootstrap';
 import Alpine from 'alpinejs';
+import intersect from '@alpinejs/intersect';
+
+// Register Alpine plugins
+Alpine.plugin(intersect);
 
 // Make Alpine available globally
 window.Alpine = Alpine;
