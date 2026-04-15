4aa1a5d621c95952a923145a046c369676778ad6
buguenocesar92 <buguenocesar92@gmail.com>
2026-04-15 10:31:23 -0400

    Add additional allowed origin for CORS configuration

--- config/cors.php
+++ config/cors.php
@@ -22,6 +22,7 @@ return [
     'allowed_origins' => [
         'http://localhost:3000',
         'http://127.0.0.1:3000',
+        'http://192.168.1.11:3000',
         'https://kraftdo-nfc-frontend.vercel.app',
         'https://app.kraftdo.cl',
         'https://app-staging.kraftdo.cl',
